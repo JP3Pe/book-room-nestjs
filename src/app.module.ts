f66ec5d19588071d4840103c0877c199e4d8507b
JP3Pe <45834750+JP3Pe@users.noreply.github.com>
2021-11-25 22:14:42 +0900

    서버 포트 동적 입력 기능 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi = require('joi');
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import AppConfig from './config/app.config';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: false,
+      load: [AppConfig],
+      // prod 환경의 환경변수는 모두 k8s가 컨트롤
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      envFilePath: process.env.NODE_ENV === 'development' ? '.env.dev' : '',
+      validationSchema: Joi.object({
+        SERVER_PORT: Joi.number().default(8008),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
