ca89c4ab261dd4e07977ae47e9d13e77a2af2e60
JP3Pe <45834750+JP3Pe@users.noreply.github.com>
2021-12-05 10:33:26 +0900

    DB 연결 설정 추가 (#4)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConnectionOptions } from "typeorm";
 import Joi = require("joi");
 
 import { AppController } from "./app.controller";
@@ -17,7 +19,19 @@ import AppConfig from "./config/app.config";
       envFilePath: process.env.NODE_ENV === "development" ? ".env.dev" : "",
       validationSchema: Joi.object({
         SERVER_PORT: Joi.number().default(8008),
+        DB_MASTER_HOST: Joi.string().required(),
+        DB_MASTER_PORT: Joi.number().required().default(3310),
+        DB_MASTER_USERNAME: Joi.string().required(),
+        DB_MASTER_PASSWORD: Joi.string().allow("").required(),
+        DB_MASTER_DATABASE: Joi.string().required().default("plane"),
       }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return configService.get<ConnectionOptions>("database");
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
